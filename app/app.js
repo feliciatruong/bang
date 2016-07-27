7a3141d3c7d9b0da49d59fffd8bb4f8910908098
Felicia Truong <xftruong@hotmail.com>
2016-07-26 21:57:20 -0700

    Remove default example

--- app/app.js
+++ app/app.js
@@ -4,14 +4,12 @@
  * This is the entry file for the application, only setup and boilerplate
  * code.
  */
-
-// Needed for redux-saga es6 generator support
 import 'babel-polyfill';
 
-// Load the favicon, the manifest.json file and the .htaccess file
-import 'file?name=[name].[ext]!./favicon.ico';
-import '!file?name=[name].[ext]!./manifest.json';
-import 'file?name=[name].[ext]!./.htaccess';
+// TODO constrain eslint import/no-unresolved rule to this block
+// Load the manifest.json file and the .htaccess file
+import '!file?name=[name].[ext]!./manifest.json';  // eslint-disable-line import/no-unresolved
+import 'file?name=[name].[ext]!./.htaccess';      // eslint-disable-line import/no-unresolved
 
 // Import all the third party stuff
 import React from 'react';
@@ -19,27 +17,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
-import FontFaceObserver from 'fontfaceobserver';
 import useScroll from 'react-router-scroll';
-import configureStore from './store';
-
-// Import Language Provider
 import LanguageProvider from 'containers/LanguageProvider';
-
-// Observe loading of Open Sans (to remove open sans, remove the <link> tag in
-// the index.html file and this observer)
-import styles from 'containers/App/styles.css';
-const openSansObserver = new FontFaceObserver('Open Sans', {});
-
-// When Open Sans is loaded, add a font-family using Open Sans to the body
-openSansObserver.load().then(() => {
-  document.body.classList.add(styles.fontLoaded);
-}, () => {
-  document.body.classList.remove(styles.fontLoaded);
-});
+import configureStore from './store';
 
 // Import i18n messages
 import { translationMessages } from './i18n';
+
+// Import the CSS reset, which HtmlWebpackPlugin transfers to the build folder
+import 'sanitize.css/sanitize.css';
 
 // Create redux store with history
 // this uses the singleton browserHistory provided by react-router
@@ -47,15 +33,6 @@ import { translationMessages } from './i18n';
 // e.g. `const browserHistory = useRouterHistory(createBrowserHistory)();`
 const initialState = {};
 const store = configureStore(initialState, browserHistory);
-
-// If you use Redux devTools extension, since v2.0.1, they added an
-// `updateStore`, so any enhancers that change the store object
-// could be used with the devTools' store.
-// As this boilerplate uses Redux & Redux-Saga, the `updateStore` is needed
-// if you want to `take` actions in your Sagas, dispatched from devTools.
-if (window.devToolsExtension) {
-  window.devToolsExtension.updateStore(store);
-}
 
 // Sync history and store, as the react-router-redux reducer
 // is under the non-default key ("routing"), selectLocationState
@@ -73,40 +50,22 @@ const rootRoute = {
   childRoutes: createRoutes(store),
 };
 
-const render = (translatedMessages) => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <LanguageProvider messages={translatedMessages}>
-        <Router
-          history={history}
-          routes={rootRoute}
-          render={
-            // Scroll to top when going to a new page, imitating default browser
-            // behaviour
-            applyRouterMiddleware(useScroll())
-          }
-        />
-      </LanguageProvider>
-    </Provider>,
-    document.getElementById('app')
-  );
-};
-
-// Hot reloadable translation json files
-if (module.hot) {
-  // modules.hot.accept does not accept dynamic dependencies,
-  // have to be constants at compile-time
-  module.hot.accept('./i18n', () => {
-    render(translationMessages);
-  });
-}
-
-// Chunked polyfill for browsers without Intl support
-if (!window.Intl) {
-  System.import('intl').then(() => render(translationMessages));
-} else {
-  render(translationMessages);
-}
+ReactDOM.render(
+  <Provider store={store}>
+    <LanguageProvider messages={translationMessages}>
+      <Router
+        history={history}
+        routes={rootRoute}
+        render={
+          // Scroll to top when going to a new page, imitating default browser
+          // behaviour
+          applyRouterMiddleware(useScroll())
+        }
+      />
+    </LanguageProvider>
+  </Provider>,
+  document.getElementById('app')
+);
 
 // Install ServiceWorker and AppCache in the end since
 // it's not most important operation and if main code fails,
